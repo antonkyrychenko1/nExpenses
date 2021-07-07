4d2d1973da31b90151dea2d722b8cc2890731f0d
Mykyta Burkovskyi <burkovskyi.work@gmail.com>
2021-07-07 20:21:16 +0300

    Add eslint and prettier

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { FC } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App: FC = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
