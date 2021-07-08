3a6086cc72fd455f3fcc08bffcd7a487c41824c3
Anton Kyrychenko <anton.kyrychenko@zwapgrid.com>
2021-07-08 21:42:58 +0300

    Add expenses creation and displaying

--- src/screens/home/home.screen.tsx
+++ src/screens/home/home.screen.tsx
@@ -1,26 +1,38 @@
 import React, { FC } from 'react';
-import { Button, Text, View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import commonStyles from '@common/styles';
 import { NavigationStackParameters } from '@app';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch } from '@store/index';
 import { AppState } from '@store/root.reducer';
-import { homeActions } from '@slices/home.slice';
+import { expensesActions } from '@slices/exprenses.slice';
+import Expense from '@models/expense';
+import uuid from '@utils/uuid-gemerator';
 
 type HomeScreenNavigationProp = StackNavigationProp<NavigationStackParameters, 'Home'>;
 
-const HomeScreen: FC<{ navigation: HomeScreenNavigationProp }> = ({ navigation }) => {
-  const count = useSelector<AppState>((state) => state.home.count);
+const HomeScreen: FC<{ navigation: HomeScreenNavigationProp }> = ({}) => {
+  const [value, onChangeText] = React.useState('');
+
+  const expenses = useSelector((state: AppState) => state.expenses.expenseList);
+
   const dispatch = useDispatch<AppDispatch>();
+  const getRowText = (e: Expense): string => e.id + ' ' + e.amount;
+
+  const displayExpenses = () => {
+    return expenses.map((e) => <Text key={e.id}>{getRowText(e)}</Text>);
+  };
 
   return (
     <View style={commonStyles.container}>
       <Text>Home Screen</Text>
-      <Text>Count: {count}</Text>
-      <Button title='Increment' onPress={() => dispatch(homeActions.increment())} />
-      <Button title='Decrement' onPress={() => dispatch(homeActions.decrement())} />
-      <Button title='Go do Details' onPress={() => navigation.navigate('Details')} />
+      <TextInput keyboardType='numeric' onChangeText={(text) => onChangeText(text)} value={value} />
+      <Button
+        title='Add expense'
+        onPress={() => dispatch(expensesActions.add({ id: uuid.generate(), amount: +value }))}
+      />
+      {displayExpenses()}
     </View>
   );
 };
